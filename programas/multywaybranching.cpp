3cd960e06b4c50d4568f3e54c1f51bea6e5cb57c
joscor34 <zbrm34@gmail.com>
2021-09-20 11:22:58 -0500

    Se agrego el programa para el estatus del estudiante

---
+++ programas/multywaybranching.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int creditsEarnet;
+
+int main()
+{
+  cout << "Ingrese la cantidad creditos tiene el estudiante:";
+  cin >> creditsEarnet;
+  if (creditsEarnet >= 90)
+    cout << "Su estatus es: SENIOR" << endl;
+  else if (creditsEarnet >= 60)
+    cout << "Su estatus es: JUNIOR" << endl;
+  else if (creditsEarnet >= 30)
+    cout << "Su estatus es: SOPHOMORE" << endl;
+  else
+    cout << "Su estatus es: FRESHMAN";
+
+  return 0;
+}
