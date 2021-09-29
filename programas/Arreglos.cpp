09d30eadb899d97e18ccd2a3c381e2173c66386a
joscor34 <zbrm34@gmail.com>
2021-09-29 10:51:15 -0500

    Se agrego la funcion par poder buscar un numero y que te regrese un contacto

--- programas/Arreglos.cpp
+++ programas/Arreglos.cpp
@@ -7,8 +7,8 @@ int main()
 {
   string N[100];
   string T[100];
-  string NAME;
-  int n, j = 0;
+  string NAME, NUMBER;
+  int n, j = 0, decision = 0;
   bool found = false;
 
   cout << "Numero de contatos: ";
@@ -29,26 +29,51 @@ int main()
     cout << "Nombre: " << N[i] << endl;
     cout << "Numero: " << T[i] << endl;
   }
-
-  cout << "Indica el nombre que desea buscar: ";
-  cin >> NAME;
-  cout << "Buscando a: " << NAME << endl;
-
-  while (found == false && j <= n)
+  cout << "Desea buscar por numero o por nombre del contacto(1 = numero, 0 = nombre): ";
+  cin >> decision;
+  if (decision == 0)
   {
-    if (NAME == N[j])
+    cout << "Indica el nombre que desea buscar: ";
+    cin >> NAME;
+    cout << "Buscando a: " << NAME << endl;
+    while (found == false && j <= n)
     {
-      cout << "El telefono del contacto es: " << T[j] << endl;
-      found = true;
+      if (NAME == N[j])
+      {
+        cout << "El telefono del contacto es: " << T[j] << endl;
+        found = true;
+      }
+      else
+      {
+        j++;
+      }
     }
-    else
+    if (found == false)
     {
-      j++;
+      cout << "No se encontró el nombre en los contactos" << endl;
     }
   }
-  if (found == false)
+  else if (decision == 1)
   {
-    cout << "No se encontró el nombre en los contactos" << endl;
+    cout << "Indica el numero que desea buscar: ";
+    cin >> NUMBER;
+    cout << "Buscando a: " << NUMBER << endl;
+    while (found == false && j <= n)
+    {
+      if (NUMBER == T[j])
+      {
+        cout << "El nombre del contacto es: " << N[j] << endl;
+        found = true;
+      }
+      else
+      {
+        j++;
+      }
+    }
+    if (found == false)
+    {
+      cout << "No se encontró el telefono en los contactos" << endl;
+    }
   }
 
   return 0;
