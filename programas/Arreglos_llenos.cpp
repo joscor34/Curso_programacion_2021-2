4853ac63aae83172fb4069b86c81577d800d360f
joscor34 <zbrm34@gmail.com>
2021-09-29 11:45:30 -0500

    Se refactorizo el codigo

--- programas/Arreglos_llenos.cpp
+++ programas/Arreglos_llenos.cpp
@@ -10,18 +10,28 @@ int main()
   string NAME, NUMBER;
   int n, j = 0, decision = 0;
   bool found = false;
+  n = 4;
+  N[0] = "Arturo";
+  T[0] = "1234";
+  N[1] = "Jose";
+  T[1] = "4567";
+  N[2] = "Caro";
+  T[2] = "7890";
+  N[3] = "Ricardo";
+  T[3] = "0987";
 
-  cout << "Numero de contatos: ";
-  cin >> n;
+  // cout << "Numero de contatos: ";
+  // cin >> n;
 
-  for (int i = 0; i < n; i++)
-  {
-    cout << "Contacto #" << i << endl;
-    cout << "Nombre: ";
-    cin >> N[i];
-    cout << "Numero: ";
-    cin >> T[i];
-  }
+  // for (int i = 0; i < n; i++)
+  // {
+  //   cout << "Contacto #" << i << endl;
+  //   cout << "Nombre: ";
+  //   cin >> N[i];
+  //   cout << "Numero: ";
+  //   cin >> T[i];
+  // }
+
   cout << "Mostrando los datos de la lista" << endl;
   cout << "---------------------" << endl;
   for (int i = 0; i < n; i++)
@@ -38,17 +48,14 @@ int main()
     cout << "Indica el nombre que desea buscar: ";
     cin >> NAME;
     cout << "Buscando a: " << NAME << endl;
-    while (found == false && j <= n)
+    while (j < n)
     {
       if (NAME == N[j])
       {
         cout << "El telefono del contacto es: " << T[j] << endl;
         found = true;
       }
-      else
-      {
-        j++;
-      }
+      j++;
     }
     if (found == false)
     {
@@ -60,17 +67,14 @@ int main()
     cout << "Indica el numero que desea buscar: ";
     cin >> NUMBER;
     cout << "Buscando a: " << NUMBER << endl;
-    while (found == false && j <= n)
+    while (j < n)
     {
       if (NUMBER == T[j])
       {
         cout << "El nombre del contacto es: " << N[j] << endl;
         found = true;
       }
-      else
-      {
-        j++;
-      }
+      j++;
     }
     if (found == false)
     {
